1fc168cd3e7aee6acb5ce20e64aa72bdcfba5235
toneiojimz <ajimeneza77@gmail.com>
2020-01-28 10:35:14 -0800

    reached MVP

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -5,30 +5,6 @@ const bc = require('bcryptjs');
 const router = express.Router();
 
 
-// router.post('/register', (req, res) => {
-//     if (req.body.username && req.body.password) {
-//         const user = req.body;
-//         user.password = bc.hashSync(req.body.password, 10);
-//         api.registerUser(user)
-//             .then(() => {
-//                 api.getUserByUsername(user.username)
-//                 .then(response => {
-//                     return res.status(201).json(response);
-//                 })
-//                 .catch(err => {
-//                     console.log(err);
-//                     return res.status(400).json({ error: "Error retrieving user after register." })
-//                 })
-//             })
-//             .catch(err => {
-//                 console.log(err);
-//                 return res.status(500).json({ error: "User was not added." })
-//             })
-//     } else {
-//         return res.status(400).json({ error: "Username and Password needed." })
-//     }
-// })
-
 router.post("/register", (req, res) => {
     let user = req.body;
 
